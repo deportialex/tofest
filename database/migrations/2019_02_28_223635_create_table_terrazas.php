6e9313cfaccb22589ca6333620438a7b3a04c797
Alejanro Mercado <deportialex@hotmail.com>
2019-02-28 16:47:47 -0600

    empezamos a crear y conectar la base de datos

---
+++ database/migrations/2019_02_28_223635_create_table_terrazas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTerrazas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terrazas', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('codigo social');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terrazas');
+    }
+}
